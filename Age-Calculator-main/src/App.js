a0a78530779045df6b4f8c4a6fd7750cc17666bc
Suraj Singh Chahar <127426634+suuraaj@users.noreply.github.com>
2023-06-19 21:51:42 +0530

    Add files via upload

---
+++ Age-Calculator-main/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import AgeCalculator from "./calculator/agecalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
